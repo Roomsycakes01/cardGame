7b4f655f54ae6657ad0f5a984db066c8050baaa3
Jake Rooms <jakeprooms@outlook.com>
2021-12-03 15:47:34 +0000

    added the magic man

--- src/Controllers/CardController/index.js
+++ src/Controllers/CardController/index.js
@@ -5,15 +5,15 @@ const ObjectId = require('mongodb').ObjectId
 
 async function displayInitialDeck(req, res){
     const processedArray = idProcessor(getBaseDeckIds())
-    const db = await connectToDatabase('cardGame')
+    const db = await connectToDatabase('cardGameAPI')
     const collection = db.collection('baseSet')
     const deck = await collection.find({_id: {$in: processedArray}}).toArray()
     res.json(deck)
 }
 
-async function displayMagicMan(req,res){
+async function displayMagicMan(req, res){
     const magicManId = ObjectId('61aa2c504b4d2d7ae4202286')
-    const db = await connectToDatabase('cardGame')
+    const db = await connectToDatabase('cardGameAPI')
     const collection = db.collection('baseSet')
     const deck = await collection.findOne({_id: magicManId})
     res.json(deck)
